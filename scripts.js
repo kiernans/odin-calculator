6213690b974b2d0bbbc2ef3f08fe17418291b02b
Kiernan Soriano <ksoriano10@horizon.csueastbay.edu>
2022-06-03 14:01:53 -0700

    Finished

--- scripts.js
+++ scripts.js
@@ -1,17 +1,18 @@
 let x = '';
 let y = '';
 let operator = '';
-let stack = [];
+let result = '';
 
 const container = document.querySelector('.container');
 const screen = document.querySelector('.screen');
 const clear = document.querySelector('.clear');
+const solve = document.querySelector('.equals');
 
 
-const sum = (x,y) => x+y;
-const subtract = (x,y) => x-y;
-const multiply = (x,y) => x*y;
-const divide = (x,y) => x/y;
+const sum = (x,y) => parseFloat(x) + parseFloat(y);
+const subtract = (x,y) => parseFloat(x) - parseFloat(y);
+const multiply = (x,y) => parseFloat(x) * parseFloat(y);
+const divide = (x,y) => parseFloat(x) / parseFloat(y);
 
 const operate = (operator, x, y) => {
     if(operator === '+') return sum(x,y);
@@ -24,40 +25,62 @@ const setScreen = (content) => {
     screen.textContent = content;
 };
 
-const getNumbers = (button) => x += button.textContent;
+const getNumbers = (button, variable) => variable += button.textContent;
 
-const getOperator = (button) => operator = button.textContent;
+const getOperator = (button, variable) => variable = button.textContent;
 
-const resetVariables = () => {
+const resetAllVariables = (bool) => {
+    if(bool) {
+        screen.textContent = '';
+        result = '';
+    }
     x = '';
     y = '';
     operator = '';
-    screen.textContent = '';
+    
 };
 
+const chooseXY = (button) => {
+    if(!operator) {
+        x = getNumbers(button, x);
+        setScreen(x);
+    } 
+    else if(operator && !x) {
+        x = result;
+    } else {
+        y = getNumbers(button, y);
+        setScreen(y);
+    }
+};
 
 const addEventListeners = () => {
     const numbers = document.querySelectorAll('.number');
     numbers.forEach((button) => {
         button.addEventListener('click', () => {
-            getNumbers(button);
-            setScreen(x);
+            chooseXY(button);
         });
     });
 
     const operators = document.querySelectorAll('.operator');
     operators.forEach((button) => {
         button.addEventListener('click', () => {
-            getOperator(button);
+            operator = getOperator(button);
             setScreen(operator);
         })
     });
 
-    clear.addEventListener('click', () => resetVariables());
+    solve.addEventListener('click', () => {
+        result = operate(operator, x, y);
+        setScreen(result);
+        resetAllVariables(false);
+    });
+
+    clear.addEventListener('click', () => resetAllVariables(true));
 }
 
+const calculator = () => {
+    addEventListeners();
+    container.insertBefore(screen, clear);
+};
 
-container.insertBefore(screen,clear);
-
-addEventListeners();
-
+calculator();
